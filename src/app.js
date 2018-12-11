6798184b8be6921bc204c33e0324004de2bd904a
Jun Ito <jun_ito@voyagegroup.com>
2018-12-11 11:46:37 +0900

    Add: 最初のシーンまで

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
 require('aframe')
+
+console.log('hey')
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-   entry: "./src/app.js",
-   output: {
-      path: path.resolve(__dirname, 'public/assets'),
-      filename: "bundle.js"
-   },
-   devServer: {
-      contentBase: path.resolve(__dirname, "public"),
-      publicPath: '/assets/',
-      watchContentBase: true
-   },
-   devtool: "source-map",
-};
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public/assets'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/assets/',
+    watchContentBase: true,
+    open: true,
+    quiet: false,
+    noInfo: false
+  },
+  devtool: 'source-map'
+}
